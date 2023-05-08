a75661cf2e9337a4b6f8fa82d6b7b5f8d67e1a90
Wilson Widyadhana <wilsonwidyadhana681@gmail.com>
2023-05-09 00:38:57 +0800

    Add border effects

--- frames.py
+++ frames.py
@@ -1,20 +1,37 @@
 import tkinter as tk
 
-window = tk.Tk()
-frame_a = tk.Frame()
-frame_b = tk.Frame()
+# window = tk.Tk()
+# frame_a = tk.Frame()
+# frame_b = tk.Frame()
 
-label_a = tk.Label(master=frame_a, text="I'm in Frame A")
-label_a.pack()
-label_b = tk.Label(master=frame_b, text="I'm in Frame B")
-label_b.pack()
+# label_a = tk.Label(master=frame_a, text="I'm in Frame A")
+# label_a.pack()
+# label_b = tk.Label(master=frame_b, text="I'm in Frame B")
+# label_b.pack()
 
 
-frame_a.pack()
-frame_b.pack()
+# frame_a.pack()
+# frame_b.pack()
 
-window.mainloop()
 
 # all four types of widgets (Label, Button, Entry, Text) have the `master` attribute you can set
 
 # frame appearance can also be adjusted 
+
+border_effects = {
+    "flat": tk.FLAT,
+    "sunken": tk.SUNKEN,
+    "raised": tk.RAISED,
+    "groove": tk.GROOVE,
+    "ridge": tk.RIDGE
+}
+
+window = tk.Tk()
+
+for relief_name, relief in border_effects.items():
+    frame = tk.Frame(master = window, relief = relief, borderwidth = 3)
+    frame.pack(side = tk.LEFT)
+    label = tk.Label(master = frame, text = relief_name)
+    label.pack(side = tk.TOP)
+
+window.mainloop()
