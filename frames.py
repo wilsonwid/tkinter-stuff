0ff7458e14da4756637b822b18c019ab39dacc4f
Wilson Widyadhana <wilsonwidyadhana681@gmail.com>
2023-05-09 00:49:51 +0800

    Add fill and side

--- frames.py
+++ frames.py
@@ -26,24 +26,40 @@ border_effects = {
     "ridge": tk.RIDGE
 }
 
-window = tk.Tk()
+# window = tk.Tk()
 
-main_frame = tk.Frame(master = window)
-for relief_name, relief in border_effects.items():
-    frame = tk.Frame(master = main_frame, relief = relief, borderwidth = 3)
-    frame.pack(side = tk.LEFT)
-    label = tk.Label(master = frame, text = relief_name)
-    label.pack(side = tk.TOP)
+# main_frame = tk.Frame(master = window)
+# for relief_name, relief in border_effects.items():
+#     frame = tk.Frame(master = main_frame, relief = relief, borderwidth = 3)
+#     frame.pack(side = tk.LEFT)
+#     label = tk.Label(master = frame, text = relief_name)
+#     label.pack(side = tk.TOP)
 
-label = tk.Label(master = window, text = "Window Label")
-label.pack(side = tk.TOP)
+# label = tk.Label(master = window, text = "Window Label")
+# label.pack(side = tk.TOP)
 
-main_frame.pack(side = tk.BOTTOM)
+# main_frame.pack(side = tk.BOTTOM)
 
-window.mainloop()
+# window.mainloop()
 
 
 # You can always do tk.Label(text = "some text").pack() and not have to worry about Python doing automatic garbage collection
 
 # If you don't choose a `side` in .pack(), then it will automatically default to tk.TOP
+
+
+window = tk.Tk()
+
+frame1 = tk.Frame(master = window, width = 200, height = 100, bg = "red")
+frame1.pack(fill = tk.Y, side = tk.LEFT) # tk.Y makes vertical resizes resopnsive; tk.X makes horizontal resizes responsive
+
+frame2 = tk.Frame(master = window, width = 100, bg = "yellow")
+frame2.pack(fill = tk.Y, side = tk.LEFT)
+
+frame3 = tk.Frame(master = window, width = 100, bg = "blue")
+frame3.pack(fill = tk.Y, side = tk.LEFT)
+
+window.mainloop()
+
+# to make resizes in both directions responsive, set the `fill` attribute to tk.BOTH and set `expand` to True
 
