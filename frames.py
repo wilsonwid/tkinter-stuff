beec5f9db643f159eaeb8ed030e4fbee14d78a20
Wilson Widyadhana <wilsonwidyadhana681@gmail.com>
2023-05-09 00:45:00 +0800

    Add tkinter positioning

--- frames.py
+++ frames.py
@@ -28,10 +28,22 @@ border_effects = {
 
 window = tk.Tk()
 
+main_frame = tk.Frame(master = window)
 for relief_name, relief in border_effects.items():
-    frame = tk.Frame(master = window, relief = relief, borderwidth = 3)
+    frame = tk.Frame(master = main_frame, relief = relief, borderwidth = 3)
     frame.pack(side = tk.LEFT)
     label = tk.Label(master = frame, text = relief_name)
     label.pack(side = tk.TOP)
 
+label = tk.Label(master = window, text = "Window Label")
+label.pack(side = tk.TOP)
+
+main_frame.pack(side = tk.BOTTOM)
+
 window.mainloop()
+
+
+# You can always do tk.Label(text = "some text").pack() and not have to worry about Python doing automatic garbage collection
+
+# If you don't choose a `side` in .pack(), then it will automatically default to tk.TOP
+
